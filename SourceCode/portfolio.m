06873eea14f2aaa327ad02dadee4fcf32a7010b9
Leon Yeo <yeolxleon@gmail.com>
2021-03-03 01:17:24 +0800

    pm1

--- SourceCode/portfolio.m
+++ SourceCode/portfolio.m
@@ -1,73 +1,22 @@
-function [output] = optimMACDH(filename)
+% AGG - iShares Core US Aggregate Bond ETF
+% SPY - SPDR S&P 500 ETF Trust
+% VGK - Vanguard European Stock Index Fund ETF
+% VWO - Vanguard Emerging Markets Stock Index Fund ETF
+
+addpath('DJIA_Prediction', 'SeroFAM', 'Indicators', 'Portfolio');
+filename = {'AGG', 'SPY', 'VGK', 'VWO'};
 
 testPercent = 0.2;
-data = fetchData(filename);
 
-[inputTrain, predOutTrain, outputTrain, ~, ...
- inputTest,  predOutTest,  outputTest,  ~] = ...
-    serofamPredict(data, testPercent, false, 1);
+opt = cell(4,1);
+data = cell(4,2);
+for i = 1:4
+    data = fetchData(filename{i});
 
-% [macdh, fMACDH] = getfMACDH(inputTest, predOutTest, 12, 26, 9, 1);
-% [rsi, fRSI] = getfRSI(inputTest, predOutTest, 14, 1);
+    [~, ~, ~, ~, inputTest,  predOutTest,  ~,  ~] = serofamPredict(data, testPercent, false, 1);
+    data{i,1} = inputTest;
+    data{i,2} = predOutTest;
 
-% peakTrough = getPeakTrough(inputTest);
-% macdhSignal = getBuySell(macdh, 0.002, -0.002);
-% fMACDHSignal = getBuySell(fMACDH, 0.002, -0.002);
-% rsiSignal = getBuySell(rsi, 60, 40);
-% fRSISignal = getBuySell(fRSI, 60, 40);
-% 
-% investMACDH = investSignal(macdhSignal, inputTest, 300000);
-% investfMACDH = investSignal(fMACDHSignal, inputTest, 300000);
-% investRSI = investSignal(rsiSignal, inputTest, 300000);
-% investfRSI = investSignal(fRSISignal, inputTest, 300000);
-% 
-% sprintf('Investment results - MACDH: %.3f, fMACDH: %.3f)', investMACDH, investfMACDH)
-% sprintf('Investment results - RSI: %.3f, fRSI: %.3f)', investRSI, investfRSI)
-
-peakTrough = getPeakTrough(inputTest);
-funcFitness = @(x) getFitnessMACDH(x, inputTest, predOutTest, peakTrough);
-lb = [1, 1, 1, 0, -0.01];
-ub = [50, 50, 50, 0.01, 0];
-A = [1, -1, 0, 0, 0];
-B = [0];
-gaOpt = optimoptions('ga');
-gaOpt.UseParallel = true;
-gaOpt.Display = 'iter';
-gaOpt.FunctionTolerance = 1e-10;
-gaOpt.MaxStallGenerations = 20;
-% x = ga(funcFitness, 8, A, B, [], [], lb, ub, [], [1,2,3,6,7,8], gaOpt);
-
-out = nan(101,6);
-
-[macdh, fMACDH, hMACDH] = getfMACDH(inputTest, predOutTest, 12, 26, 9, 1);
-
-hMACDHSignal = getBuySell(hMACDH, 0, 0);
-[lagBuy, lagSell, miss, total] = getLag(peakTrough, hMACDHSignal);
-investhMACDH = investSignal(hMACDHSignal, inputTest, 300000);
-out(1,:) = [investhMACDH, lagBuy, lagSell, miss, total, nan(1,1)];
-
-macdhSignal = getBuySell(macdh, 0.002, -0.002);
-fMACDHSignal = getBuySell(fMACDH, 0.002, -0.002);
-[lagBuy, lagSell, miss, total] = getLag(peakTrough, fMACDHSignal);
-
-investMACDH = investSignal(macdhSignal, inputTest, 300000);
-investfMACDH = investSignal(fMACDHSignal, inputTest, 300000);
-out(2,:) = [investMACDH, investfMACDH, lagBuy, lagSell, miss, total];
-
-for i = 3:102
-    x = ga(funcFitness, 5, A, B, [], [], lb, ub, [], [1,2,3], gaOpt);
-    [macdh, fMACDH, ~] = getfMACDH(inputTest, predOutTest, x(1), x(2), x(3), 1);
-    macdhSignal = getBuySell(macdh, x(4), x(5));
-    fMACDHSignal = getBuySell(fMACDH, x(4), x(5));
-    [lagBuy, lagSell, miss, total] = getLag(peakTrough, fMACDHSignal);
-
-    investMACDH = investSignal(macdhSignal, inputTest, 300000);
-    investfMACDH = investSignal(fMACDHSignal, inputTest, 300000);
-    out(i, :) = [investMACDH, investfMACDH, lagBuy, lagSell, miss, total];
-end
-
-bestCase = investSignal(peakTrough, inputTest, 300000);
-sprintf('Best case: %.3f', bestCase)
-
-output = out;
+    bnh = investBuyAndHold(inputTest, 1e6);
+    sprintf("%s buy and hold result: %.3f", filename{i}, bnh)
 end
